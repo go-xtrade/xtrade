318f0f70ffe3825d1c22fd6e020e83760c8413ad
liwei <wei9.li@changhong.com>
2017-08-06 19:36:40 +0800

    copy lib to /usr/local/lib/

--- main.go
+++ main.go
@@ -12,8 +12,19 @@ func main() {
 	as := api.GetStore()
 	go func(as *api.Store) {
 		var uapi *api.GoTraderAPI
-		uapi = api.NewTraderAPI("tcp://180.168.146.187:10000", "9999", "your id", "your password")
-		as.Set("099035", uapi)
+		uapi = api.NewTraderAPI("tcp://180.168.146.187:10000", "9999", "<your accout 1>", "your password")
+		as.Set("your accout 1", uapi)
+		fmt.Printf("API store size:%d\n", as.Size())
+		spi := api.NewTraderSPI()
+		uapi.RegisterSpi(spi)
+		uapi.RegisterFront()
+		uapi.Connect()
+		uapi.Join()
+	}(as)
+	go func(as *api.Store) {
+		var uapi *api.GoTraderAPI
+		uapi = api.NewTraderAPI("tcp://180.168.146.187:10001", "9999", "<your accout 2>", "your password")
+		as.Set("your accout 2", uapi)
 		fmt.Printf("API store size:%d\n", as.Size())
 		spi := api.NewTraderSPI()
 		uapi.RegisterSpi(spi)
